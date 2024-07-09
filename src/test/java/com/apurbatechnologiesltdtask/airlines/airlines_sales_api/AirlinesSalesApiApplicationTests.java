71a45b943c56f147b3380d4c21b269b03446addd
ahsanulSowmik <ahsanulsowmik2@gmail.com>
2024-07-10 01:24:27 +0600

    #Project Set Up.

---
+++ src/test/java/com/apurbatechnologiesltdtask/airlines/airlines_sales_api/AirlinesSalesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apurbatechnologiesltdtask.airlines.airlines_sales_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlinesSalesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
